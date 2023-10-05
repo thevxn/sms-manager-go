d571ee858ed94346e99d85e6b137612293955606
krustowski <krusty@savla.dev>
2023-10-05 11:32:39 +0300

    add metrics export body composition fix for PushGateway

--- example/flags/main.go
+++ example/flags/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	client "go.savla.dev/sms-manager/pkg/client"
@@ -32,21 +33,23 @@ func main() {
 	// do the request
 	if err := client.DoRequest(req, config.MethodName, resp); err != nil {
 		log.Fatal(err)
+		os.Exit(1)
 	}
 
 	// print method's response fields --- only raw credit balance there for example
 	if resp.Message != "ERROR" {
 		//fmt.Printf("credit left : %.2f CZK (exc. VAT)\n\r", resp.CreditBalance)
-		fmt.Printf("%.2f", resp.CreditBalance)
+		//fmt.Printf("%.2f", resp.CreditBalance)
 	} else {
 		fmt.Printf("response error code: %d\n\r", resp.ErrorCode)
 		fmt.Println("response error msq : " + resp.ErrorMessage)
+		os.Exit(2)
 	}
 
 	// compose a request body
 	body := fmt.Sprintln("#HELP SMS Manager available credit balance")
 	body += fmt.Sprintln("#TYPE credit_balance_czk_total gauge")
-	body += fmt.Sprintln("credit balance_czk_total", strconv.FormatFloat(resp.CreditBalance, 'f', 0, 64))
+	body += fmt.Sprintln("credit_balance_czk_total", strconv.FormatFloat(resp.CreditBalance, 'f', 2, 64))
 
 	bodyReader := bytes.NewReader([]byte(body))
 
@@ -54,14 +57,16 @@ func main() {
 	request, err := http.NewRequest("PUT", config.PushGatewayURL, bodyReader)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(3)
 	}
-	request.Header.Set("Content-Type", "application/octet-stream")
+	request.Header.Set("Content-Type", "application/json")
 
 	client := http.Client{}
 
 	res, err := client.Do(request)
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(4)
 	}
 
 	log.Println("Results pushed to pushgateway")
